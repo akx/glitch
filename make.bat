00938a7d596d2c5be6b106cb80cb13780ea66755
Aarni Koskela <akx@iki.fi>
2013-07-06 10:41:45 +0300

    Add bitbang effect

--- make.bat
+++ make.bat
@@ -1,1 +1,2 @@
-uglifyjs StackBlur.js context_blender.js dat.gui.js glitch.js -m -c > _all.js
+call coco -c glitch.co
+call uglifyjs StackBlur.js context_blender.js dat.gui.js glitch.js -m -c > _all.js
